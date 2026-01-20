68960d8366cdce42a7ff5fd51e80b065935d2579
CharlyMaye <charlymayeux@hotmail.fr>
2026-01-20 05:13:50 +0000

    feat: update WASM resource handling and improve loading states in home component

--- test-neural/ai-web-app/src/app/app.ts
+++ test-neural/ai-web-app/src/app/app.ts
@@ -1,7 +1,5 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WasmService } from '@cma/wasm/shared';
-import init, { XorNetwork} from '@cma/wasm/xor_wasm/neural_wasm_xor.js';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +8,4 @@ import init, { XorNetwork} from '@cma/wasm/xor_wasm/neural_wasm_xor.js';
   styleUrl: './app.scss'
 })
 export class App {
-  private readonly wasmService = inject(WasmService);
-  public readonly xorInitOutput = this.wasmService.xorWasmResource.value.asReadonly();
-  public readonly irisInitOutput = this.wasmService.irisWasmResource.value.asReadonly();
-
-  constructor() {
-    effect  (() => {
-      console.log('WASM Resource loaded:', this.xorInitOutput());
-    });
-    effect  (() => {
-      console.log('WASM Resource loaded:', this.irisInitOutput());
-    });
-  }
 }
--- test-neural/ai-web-app/src/app/pages/home/home.ts
+++ test-neural/ai-web-app/src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
-import { Component, signal, Signal } from '@angular/core';
+import { Component, effect, inject, signal, Signal } from '@angular/core';
 import { PageTitle, PageTitleOptions } from '../../ui/page-title/page-title';
 import { PageFooter } from '../../ui/page-footer/page-footer';
 import { About } from '../../ui/about/about';
 import { RouterLinkWithHref } from '@angular/router';
+import { WasmService } from '@cma/wasm/shared/wasm';
 
 
 @Component({
@@ -12,15 +13,27 @@ import { RouterLinkWithHref } from '@angular/router';
   styleUrl: './home.scss',
 })
 export class Home {
+  private readonly wasmService = inject(WasmService);
+  public readonly xorIsLoading = this.wasmService.xorWasmResource.isLoading;
+  public readonly irisIsLoading = this.wasmService.irisWasmResource.isLoading;
+
   public readonly pageTitleOptions: Signal<PageTitleOptions> = signal({
     title: 'Neural Networks',
     subtitle: 'Interactive WebAssembly Demos',
     icon: '🧠',
   });
 
-  constructor() {
-  }
+  public readonly xorInitOutput = this.wasmService.xorWasmResource.value.asReadonly();
+  public readonly irisInitOutput = this.wasmService.irisWasmResource.value.asReadonly();
 
+  constructor() {
+    effect  (() => {
+      console.log('WASM Resource loaded:', this.xorInitOutput());
+    });
+    effect  (() => {
+      console.log('WASM Resource loaded:', this.irisInitOutput());
+    });
+  }
   public ngOnInit() {
     this.loadModelAccuracies();
   }
