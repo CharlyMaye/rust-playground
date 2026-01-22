7dec7337a3941db60d36d866a73d66d0e2cbb720
CharlyMaye <charlymayeux@hotmail.fr>
2026-01-22 04:51:51 +0000

    feat: implement model info component for Iris and XOR classifiers; refactor UI to utilize new component and improve loading states

--- test-neural/ai-web-app/src/app/pages/xor-logic-gate/xor-logic-gate.ts
+++ test-neural/ai-web-app/src/app/pages/xor-logic-gate/xor-logic-gate.ts
@@ -4,10 +4,11 @@ import { PageFooter } from '../../ui/page-footer/page-footer';
 import { NavigationBack } from '../../ui/navigation-back/navigation-back';
 import { Loader } from '../../ui/loader/loader';
 import { WasmFacade } from '@cma/wasm/shared/wasm';
+import { ModelInfoComponent } from '../../ui/model-info/model-info';
 
 @Component({
   selector: 'app-xor-logic-gate',
-  imports: [PageTitle, Loader, NavigationBack, PageFooter],
+  imports: [PageTitle, Loader, NavigationBack, ModelInfoComponent, PageFooter],
   templateUrl: './xor-logic-gate.html',
   styleUrl: './xor-logic-gate.scss',
 })
