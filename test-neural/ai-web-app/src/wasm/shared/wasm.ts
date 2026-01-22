489914747bcac0e8f371d69b880031ec77067078
CharlyMaye <charlymayeux@hotmail.fr>
2026-01-22 18:17:56 +0000

    feat: update WASM resource handling and improve loading states in home component

--- test-neural/ai-web-app/src/wasm/shared/wasm.ts
+++ test-neural/ai-web-app/src/wasm/shared/wasm.ts
@@ -10,20 +10,21 @@ export class WasmService {
   public readonly xorWasmResource: ResourceRef<InitXorOutput | undefined> = resource({
     loader: async () => {
       const wasmlPath = '/wasm/xor_wasm/neural_wasm_xor_bg.wasm';
-      const fetchResponse = await fetch(wasmlPath);
-      console.log('status:', fetchResponse.status);
+      // const fetchResponse = await fetch(wasmlPath);
       const initResponse = await init(wasmlPath);
       return initResponse;            
-    }
+    },
+    defaultValue: undefined,
   });
   public readonly irisWasmResource: ResourceRef<InitIraisOutput | undefined> = resource({
     loader: async () => {
       const wasmlPath = '/wasm/iris_wasm/neural_wasm_iris_bg.wasm';
-      const fetchResponse = await fetch(wasmlPath);
-      console.log('status:', fetchResponse.status);
+      // const fetchResponse = await fetch(wasmlPath);
+      
       const initResponse = await initIris(wasmlPath);
       return initResponse;            
-    }
+    },
+    defaultValue: undefined,
   });
   
 }
